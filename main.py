0944a2c1cc484387b6c0acafa18034ffa068ec4e
jhi2 <johndrostan@outlook.com>
2025-11-04 19:52:54 -0800

    Okey Dokey. copied

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+if __name__ == '__main__':
+	# Run the development server; open http://127.0.0.1:5000/ in your browser
+	app.run(debug=True)
